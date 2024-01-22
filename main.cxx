d668d569748a9e82ddac5af43338984acd9c075e
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2024-01-22 13:30:39 +0900

    cosmetic change

--- main.cxx
+++ main.cxx
@@ -1,21 +1,18 @@
 #include <algorithm>
-#include <cctype>
 #include <exception>
 #include <iostream>
-#include <stdexcept>
 #include <string>
 
 #include <libbech32/bech32.h>
 #include <nlohmann/json.hpp>
 #include <openssl/evp.h>
-#include <openssl/x509v3.h>
 
 #include <secp256k1.h>
 #include <secp256k1_schnorrsig.h>
 
 #include <Server.h>
 
-typedef struct {
+typedef struct event_t {
   std::string id;
   std::string pubkey;
   std::time_t created_at;
@@ -25,9 +22,7 @@ typedef struct {
   std::string sig;
 } event_t;
 
-std::vector<event_t> events;
-
-typedef struct {
+typedef struct filter_t {
   std::vector<std::string> ids;
   std::vector<std::string> authors;
   std::vector<int> kinds;
@@ -36,17 +31,17 @@ typedef struct {
   std::time_t until;
 } filter_t;
 
-typedef struct {
-  ws28::Client *client;
+typedef struct subscriber_t {
+  ws28::Client *client{};
   std::vector<filter_t> filters;
 } subscriber_t;
 
 std::map<std::string, subscriber_t> subscribers;
 
-static std::string bytes2hex(const uint8_t *data, int len) {
+static std::string bytes2hex(const uint8_t *data, const int len) {
   std::stringstream ss;
   ss << std::hex;
-  for (int i(0); i < len; ++i)
+  for (int i = 0; i < len; ++i)
     ss << std::setw(2) << std::setfill('0') << (int)data[i];
   return ss.str();
 }
@@ -55,39 +50,43 @@ static std::vector<uint8_t> hex2bytes(const std::string &hex) {
   std::vector<uint8_t> bytes;
   for (unsigned int i = 0; i < hex.length(); i += 2) {
     std::string s = hex.substr(i, 2);
-    uint8_t byte = (uint8_t)strtol(s.c_str(), nullptr, 16);
+    auto byte = (uint8_t)strtol(s.c_str(), nullptr, 16);
     bytes.push_back(byte);
   }
   return bytes;
 }
 
-static void relay_send(ws28::Client *client, nlohmann::json& data) {
+static void relay_send(ws28::Client *client, nlohmann::json &data) {
   auto s = data.dump();
   std::cout << "<< " << s << std::endl;
   client->Send(s.data(), s.size());
 }
 
-static void relay_final(ws28::Client *client, std::string id, std::string msg) {
+static void relay_final(ws28::Client *client, const std::string &id,
+                        const std::string &msg) {
   nlohmann::json data = {"CLOSED", id, msg};
   relay_send(client, data);
   client->Close(0);
 }
 
-static bool signature_verify(secp256k1_context *ctx,
-                             const std::vector<uint8_t> &bytessig,
+static bool signature_verify(const std::vector<uint8_t> &bytessig,
                              const std::vector<uint8_t> &bytespub,
                              const uint8_t *digest) {
+  secp256k1_context *ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN |
+                                                    SECP256K1_CONTEXT_VERIFY);
   secp256k1_xonly_pubkey pub;
-  secp256k1_ecdsa_signature sig;
   if (!secp256k1_xonly_pubkey_parse(ctx, &pub, bytespub.data())) {
+    secp256k1_context_destroy(ctx);
     return false;
   }
 
-  return secp256k1_schnorrsig_verify(ctx, bytessig.data(), digest,
+  auto result = secp256k1_schnorrsig_verify(ctx, bytessig.data(), digest,
 #ifdef SECP256K1_SCHNORRSIG_EXTRAPARAMS_INIT
-                                     32,
+                                            32,
 #endif
-                                     &pub);
+                                            &pub);
+  secp256k1_context_destroy(ctx);
+  return result;
 }
 
 static void do_relay_req(ws28::Client *client, nlohmann::json &data) {
@@ -106,17 +105,17 @@ static void do_relay_req(ws28::Client *client, nlohmann::json &data) {
         .until = 0,
     };
     if (data[i].count("ids") > 0) {
-      for (const auto id : data[i]["ids"]) {
+      for (const auto &id : data[i]["ids"]) {
         filter.ids.push_back(id);
       }
     }
     if (data[i].count("authors") > 0) {
-      for (const auto author : data[i]["authors"]) {
+      for (const auto &author : data[i]["authors"]) {
         filter.authors.push_back(author);
       }
     }
     if (data[i].count("kinds") > 0) {
-      for (const auto kind : data[i]["kinds"]) {
+      for (const auto &kind : data[i]["kinds"]) {
         filter.authors.push_back(kind);
       }
     }
@@ -124,7 +123,7 @@ static void do_relay_req(ws28::Client *client, nlohmann::json &data) {
          ++it) {
       if (it.key().at(0) == '#' && it.value().is_array()) {
         std::vector<std::string> tag = {it.key().c_str() + 1};
-        for (const auto v : it.value()) {
+        for (const auto &v : it.value()) {
           tag.push_back(v);
         }
         filter.tags.push_back(tag);
@@ -219,7 +218,7 @@ static bool matched_filters(const std::vector<filter_t> &filters,
 
 static void do_relay_event(ws28::Client *client, nlohmann::json &data) {
   auto ej = data[1];
-  event_t ev;
+  event_t ev = {};
   try {
     ev.id = ej["id"];
     ev.pubkey = ej["pubkey"];
@@ -237,30 +236,26 @@ static void do_relay_event(ws28::Client *client, nlohmann::json &data) {
     EVP_Digest(dump.data(), dump.size(), digest, nullptr, EVP_sha256(),
                nullptr);
 
-    auto id = bytes2hex(digest, 32);
+    auto id = bytes2hex(digest, sizeof(digest));
     if (id != ev.id) {
       relay_final(client, "", "error: invalid id");
       return;
     }
 
-    secp256k1_context *ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN |
-                                                      SECP256K1_CONTEXT_VERIFY);
     auto bytessig = hex2bytes(ev.sig);
     auto bytespub = hex2bytes(ev.pubkey);
-    if (!signature_verify(ctx, bytessig, bytespub, digest)) {
-      secp256k1_context_destroy(ctx);
+    if (!signature_verify(bytessig, bytespub, digest)) {
       relay_final(client, "", "error: invalid signature");
       return;
     }
-    secp256k1_context_destroy(ctx);
 
     for (const auto &s : subscribers) {
       if (id == s.first) {
         continue;
       }
       if (matched_filters(s.second.filters, ev)) {
-        nlohmann::json data = {"EVENT", s.first, ej};
-        relay_send(s.second.client, data);
+        nlohmann::json reply = {"EVENT", s.first, ej};
+        relay_send(s.second.client, reply);
       }
     }
     relay_final(client, "", "OK");
@@ -288,14 +283,15 @@ static void close_callback(ws28::Client *client) {
 }
 
 static void data_callback(ws28::Client *client, char *data, size_t len,
-                          int opcode) {
+                          int /*opcode*/) {
   std::string s;
   s.append(data, len);
   std::cout << ">> " << s << std::endl;
   auto payload = nlohmann::json::parse(s);
 
   if (!payload.is_array() || payload.size() < 2) {
-    nlohmann::json data = {"CLOSED", "", "error: invalid request"};
+    relay_final(client, "", "error: invalid request");
+    return;
   }
 
   std::string method = payload[0];
@@ -324,11 +320,11 @@ static void data_callback(ws28::Client *client, char *data, size_t len,
   client->Close(0);
 }
 
-static void signal_handler(uv_signal_t *req, int signum) {
+static void signal_handler(uv_signal_t *req, int /*signum*/) {
   uv_signal_stop(req);
   std::cerr << "!! SIGINT" << std::endl;
-  for (auto it = subscribers.begin(); it != subscribers.end(); ++it) {
-    relay_final(it->second.client, it->first, "shutdown...");
+  for (const auto &[key, value] : subscribers) {
+    relay_final(value.client, key, "shutdown...");
   }
 
   uv_stop(uv_default_loop());
