b2111482a3a8983e1847e38781517db99618eb3e
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2024-01-22 23:20:08 +0900

    implement sqlite3 storage

--- main.cxx
+++ main.cxx
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <exception>
 #include <iostream>
+#include <sstream>
 #include <string>
 
 #include <libbech32/bech32.h>
@@ -10,7 +11,15 @@
 #include <secp256k1.h>
 #include <secp256k1_schnorrsig.h>
 
+#include <sqlite3.h>
+
 #include <Server.h>
+
+class tags_t {
+public:
+  std::vector<std::vector<std::string>> values;
+  void print() {}
+};
 
 typedef struct event_t {
   std::string id;
@@ -36,13 +45,16 @@ typedef struct subscriber_t {
   std::vector<filter_t> filters;
 } subscriber_t;
 
+// global variables
 std::map<std::string, subscriber_t> subscribers;
+sqlite3 *conn = NULL;
 
-static std::string bytes2hex(const uint8_t *data, const int len) {
+static std::string digest2hex(const uint8_t *data) {
   std::stringstream ss;
   ss << std::hex;
-  for (int i = 0; i < len; ++i)
+  for (int i = 0; i < 32; ++i) {
     ss << std::setw(2) << std::setfill('0') << (int)data[i];
+  }
   return ss.str();
 }
 
@@ -69,24 +81,104 @@ static void relay_final(ws28::Client *client, const std::string &id,
   client->Close(0);
 }
 
-static bool signature_verify(const std::vector<uint8_t> &bytessig,
-                             const std::vector<uint8_t> &bytespub,
+static bool signature_verify(const std::vector<uint8_t> &bytes_sig,
+                             const std::vector<uint8_t> &bytes_pub,
                              const uint8_t *digest) {
-  secp256k1_context *ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN |
-                                                    SECP256K1_CONTEXT_VERIFY);
+#define secp256k1_context_flags                                                \
+  (SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY)
+  secp256k1_context *ctx = secp256k1_context_create(secp256k1_context_flags);
   secp256k1_xonly_pubkey pub;
-  if (!secp256k1_xonly_pubkey_parse(ctx, &pub, bytespub.data())) {
+  if (!secp256k1_xonly_pubkey_parse(ctx, &pub, bytes_pub.data())) {
     secp256k1_context_destroy(ctx);
     return false;
   }
 
-  auto result = secp256k1_schnorrsig_verify(ctx, bytessig.data(), digest,
+  auto result = secp256k1_schnorrsig_verify(ctx, bytes_sig.data(), digest,
 #ifdef SECP256K1_SCHNORRSIG_EXTRAPARAMS_INIT
                                             32,
 #endif
                                             &pub);
   secp256k1_context_destroy(ctx);
   return result;
+}
+
+std::string make_in_query(const std::string name, const nlohmann::json& data) {
+  auto s = data.dump();
+  s = s.substr(1, s.size()-2);
+  return " " + name + " in (" + s+ ")";
+}
+
+static bool send_records(ws28::Client *client, std::string &sub,
+                         std::vector<filter_t> &filters) {
+  std::string sql = R"(
+    SELECT
+      id, pubkey, created_at, kind, tags, content, sig
+    FROM event WHERE 1 = 1
+  )";
+
+  for (const auto &filter : filters) {
+    if (!filter.ids.empty()) {
+      sql += " AND " + make_in_query("id", filter.ids);
+    }
+    if (!filter.authors.empty()) {
+      sql += " AND " + make_in_query("pubkey", filter.authors);
+    }
+    if (!filter.kinds.empty()) {
+      sql += " AND " + make_in_query("kinds", filter.kinds);
+    }
+    if (!filter.tags.empty()) {
+      std::string condition = "(";
+      for (const auto tag : filter.tags) {
+        nlohmann::json data = tag;
+        auto s = data.dump();
+        if (!condition.empty()) {
+          condition += " OR ";
+        }
+        condition += "tags LIKE %" + s + "%";
+      }
+      condition += ")";
+      sql += " AND " + condition;
+    }
+    if (filter.since != 0) {
+      std::ostringstream os;
+      os << filter.since;
+      sql += " AND created_at >= " + os.str();
+    }
+    if (filter.until != 0) {
+      std::ostringstream os;
+      os << filter.until;
+      sql += " AND created_at <= " + os.str();
+    }
+  }
+
+  sqlite3_stmt *stmt = nullptr;
+  // std::cout << sql << std::endl;
+  auto ret = sqlite3_prepare(conn, sql.data(), sql.size(), &stmt, nullptr);
+  if (ret != SQLITE_OK) {
+    fprintf(stderr, "%s\n", sqlite3_errmsg(conn));
+    return false;
+  }
+  while (true) {
+    ret = sqlite3_step(stmt);
+    if (ret == SQLITE_DONE) {
+      break;
+    }
+    nlohmann::json ej;
+    ej["id"] = (char *)sqlite3_column_text(stmt, 0);
+    ej["pubkey"] = (char *)sqlite3_column_text(stmt, 1);
+    ej["created_at"] = sqlite3_column_int(stmt, 2);
+    ej["kind"] = sqlite3_column_int(stmt, 3);
+    const unsigned char *j = sqlite3_column_text(stmt, 4);
+    ej["tags"] = nlohmann::json::parse(j);
+    ej["content"] = (char *)sqlite3_column_text(stmt, 5);
+    ej["sig"] = (char *)sqlite3_column_text(stmt, 6);
+
+    nlohmann::json reply = {"EVENT", sub, ej};
+    relay_send(client, reply);
+  }
+  sqlite3_finalize(stmt);
+
+  return true;
 }
 
 static void do_relay_req(ws28::Client *client, nlohmann::json &data) {
@@ -138,8 +230,10 @@ static void do_relay_req(ws28::Client *client, nlohmann::json &data) {
     filters.push_back(filter);
   }
   subscribers[sub] = {.client = client, .filters = filters};
-  auto eose = nlohmann::json::array({"EOSE", sub});
-  relay_send(client, eose);
+
+  send_records(client, sub, filters);
+  auto reply = nlohmann::json::array({"EOSE", sub});
+  relay_send(client, reply);
 }
 
 static void do_relay_close(ws28::Client *client, nlohmann::json &data) {
@@ -195,10 +289,10 @@ static bool matched_filters(const std::vector<filter_t> &filters,
       for (const auto &tag : ev.tags) {
         if (tag.size() < 2)
           continue;
-        for (const auto &mtag : filter.tags) {
-          if (mtag.size() < 2)
+        for (const auto &filter_tag : filter.tags) {
+          if (filter_tag.size() < 2)
             continue;
-          if (tag == mtag) {
+          if (tag == filter_tag) {
             matched = true;
             break;
           }
@@ -214,6 +308,38 @@ static bool matched_filters(const std::vector<filter_t> &filters,
     found = true;
   }
   return found;
+}
+
+static bool insert_record(event_t &ev) {
+  const auto sql = R"(
+    INSERT INTO event (id, pubkey, created_at, kind, tags, content, sig)
+    VALUES ($1, $2, $3, $4, $5, $6, $7)
+  )";
+  sqlite3_stmt *stmt = nullptr;
+  auto ret = sqlite3_prepare(conn, sql, strlen(sql), &stmt, nullptr);
+  if (ret != SQLITE_OK) {
+    fprintf(stderr, "%s\n", sqlite3_errmsg(conn));
+    return false;
+  }
+  nlohmann::json tags = ev.tags;
+  auto s = tags.dump();
+  sqlite3_bind_text(stmt, 1, ev.id.data(), ev.id.size(), nullptr);
+  sqlite3_bind_text(stmt, 2, ev.pubkey.data(), ev.pubkey.size(), nullptr);
+  sqlite3_bind_int(stmt, 3, ev.created_at);
+  sqlite3_bind_int(stmt, 4, ev.kind);
+  sqlite3_bind_text(stmt, 5, s.data(), s.size(), nullptr);
+  sqlite3_bind_text(stmt, 6, ev.content.data(), ev.content.size(), nullptr);
+  sqlite3_bind_text(stmt, 7, ev.sig.data(), ev.sig.size(), nullptr);
+
+  ret = sqlite3_step(stmt);
+  if (ret != SQLITE_DONE) {
+    fprintf(stderr, "%s\n", sqlite3_errmsg(conn));
+    sqlite3_free(stmt);
+    return false;
+  }
+  sqlite3_finalize(stmt);
+
+  return true;
 }
 
 static void do_relay_event(ws28::Client *client, nlohmann::json &data) {
@@ -236,16 +362,21 @@ static void do_relay_event(ws28::Client *client, nlohmann::json &data) {
     EVP_Digest(dump.data(), dump.size(), digest, nullptr, EVP_sha256(),
                nullptr);
 
-    auto id = bytes2hex(digest, sizeof(digest));
+    auto id = digest2hex(digest);
     if (id != ev.id) {
       relay_final(client, "", "error: invalid id");
       return;
     }
 
-    auto bytessig = hex2bytes(ev.sig);
-    auto bytespub = hex2bytes(ev.pubkey);
-    if (!signature_verify(bytessig, bytespub, digest)) {
+    auto bytes_sig = hex2bytes(ev.sig);
+    auto bytes_pub = hex2bytes(ev.pubkey);
+    if (!signature_verify(bytes_sig, bytes_pub, digest)) {
       relay_final(client, "", "error: invalid signature");
+      return;
+    }
+
+    if (!insert_record(ev)) {
+      relay_final(client, "", "error: duplicate event");
       return;
     }
 
@@ -330,7 +461,38 @@ static void signal_handler(uv_signal_t *req, int /*signum*/) {
   uv_stop(uv_default_loop());
 }
 
-int main(int argc, char *argv[]) {
+static void storage_init() {
+  auto ret = sqlite3_open("./cagliostr.sqlite", &conn);
+  if (ret != SQLITE_OK) {
+    fprintf(stderr, "%s\n", sqlite3_errmsg(conn));
+    exit(-1);
+  }
+
+  const auto sql = R"(
+	CREATE TABLE IF NOT EXISTS event (
+       id text NOT NULL,
+       pubkey text NOT NULL,
+       created_at integer NOT NULL,
+       kind integer NOT NULL,
+       tags jsonb NOT NULL,
+       content text NOT NULL,
+       sig text NOT NULL);
+	CREATE UNIQUE INDEX IF NOT EXISTS ididx ON event(id);
+	CREATE INDEX IF NOT EXISTS pubkeyprefix ON event(pubkey);
+	CREATE INDEX IF NOT EXISTS timeidx ON event(created_at DESC);
+	CREATE INDEX IF NOT EXISTS kindidx ON event(kind);
+	CREATE INDEX IF NOT EXISTS kindtimeidx ON event(kind,created_at DESC);
+  )";
+  ret = sqlite3_exec(conn, sql, nullptr, nullptr, nullptr);
+  if (ret != SQLITE_OK) {
+    fprintf(stderr, "%s\n", sqlite3_errmsg(conn));
+    exit(-1);
+  }
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
+  storage_init();
+
   uv_loop_t *loop = uv_default_loop();
   ws28::Server server{loop, nullptr};
   server.SetClientDataCallback(data_callback);
